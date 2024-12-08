a2bd78a7a9455ba9b6a2790f460699630fbe4031
bimbims125 <pramvendread@gmail.com>
2024-12-08 13:42:01 +0700

    Feat(Payment): Integrate and create service for Payment using Midtrans

--- internal/delivery/http/handler/order_handler.go
+++ internal/delivery/http/handler/order_handler.go
@@ -10,28 +10,67 @@ import (
 )
 
 type OrderHandler struct {
-	usecase *usecase.OrderUsecase
+	usecase         *usecase.OrderUsecase
+	midtransUsecase *usecase.MidtransUsecase
+	userUsecase     *usecase.UserUseCase
+	productUsecase  *usecase.ProductUseCase
 }
 
-func NewOrderHandler(usecase *usecase.OrderUsecase) *OrderHandler {
-	return &OrderHandler{usecase: usecase}
+func NewOrderHandler(usecase *usecase.OrderUsecase, midtransUsecase *usecase.MidtransUsecase, userUsecase *usecase.UserUseCase, productUsecase *usecase.ProductUseCase) *OrderHandler {
+	return &OrderHandler{usecase: usecase, midtransUsecase: midtransUsecase, userUsecase: userUsecase, productUsecase: productUsecase}
 }
 
 func (h *OrderHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
+
 	var order entity.Order
 	if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	product, err := h.productUsecase.GetProductByID(r.Context(), order.ProductID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user, err := h.userUsecase.GetUserByID(r.Context(), order.UserID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	getOrder := entity.GetOrder{
+		User:     entity.UserOrder{ID: user.ID, Name: user.Name, Email: user.Email},
+		Product:  entity.ProductOrder{ID: product.ID, Name: product.Name, Price: product.Price},
+		Quantity: order.Quantity,
+	}
+
+	// Total amount
+	totalAmount := getOrder.Product.Price * float64(order.Quantity)
+
+	midtransResponse, err := h.midtransUsecase.CreatePayment(getOrder, entity.MidtransRequest{UserID: order.UserID, Amount: int64(totalAmount),
+		ItemID: order.ProductID, ItemName: getOrder.Product.Name})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println(totalAmount)
+		return
+	}
+
+	// Transaction ID
+	order.TransactionID = midtransResponse.TransactionID
+
+	// Total Price
+	order.TotalPrice = totalAmount
+
 	id, err := h.usecase.CreateOrder(r.Context(), &order)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		fmt.Println(err)
 		return
 	}
-	utils.JSONResponse(w, http.StatusCreated, map[string]interface{}{
-		"id": id,
-	})
+	fmt.Print(id)
+	utils.JSONResponse(w, http.StatusCreated, midtransResponse)
 }
 
 func (h *OrderHandler) GetOrders(w http.ResponseWriter, r *http.Request) {
--- internal/delivery/http/routes/order_routes.go
+++ internal/delivery/http/routes/order_routes.go
@@ -7,8 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func RegisterOrderRoutes(router *mux.Router, uc *usecase.OrderUsecase) {
-	orderHandler := handler.NewOrderHandler(uc)
+func RegisterOrderRoutes(router *mux.Router, uc *usecase.OrderUsecase, midtransUsecase *usecase.MidtransUsecase, userUseCase *usecase.UserUseCase, productUsecase *usecase.ProductUseCase) {
+	orderHandler := handler.NewOrderHandler(uc, midtransUsecase, userUseCase, productUsecase)
 	router.HandleFunc("/orders", orderHandler.CreateOrder).Methods("POST")
 	router.HandleFunc("/orders", orderHandler.GetOrders).Methods("GET")
 }
