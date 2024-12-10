a54511a7ccfe3b9deecb833ce895b4dc320ee4e0
bimbims125 <pramvendread@gmail.com>
2024-12-10 15:08:12 +0700

    Feat(Auth): Create auth JWT

--- internal/delivery/http/handler/user_handler.go
+++ internal/delivery/http/handler/user_handler.go
@@ -3,9 +3,11 @@ package handler
 import (
 	"encoding/json"
 	"gocommerce/internal/entity"
+	"gocommerce/internal/infra"
 	"gocommerce/internal/usecase"
 	"gocommerce/internal/utils"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
@@ -33,6 +35,34 @@ func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"id": id,
+	})
+}
+
+func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var payload entity.User
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user, err := h.usecase.GetUserByEmail(r.Context(), payload.Email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	if !utils.ComparePasswords(user.Password, []byte(payload.Password)) {
+		utils.JSONResponse(w, http.StatusUnauthorized, map[string]interface{}{"message": "Invalid password!"})
+		return
+	}
+
+	token, err := infra.GenerateJWT([]byte(os.Getenv("JWT_SECRET")), user.ID, user.Role)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	utils.JSONResponse(w, http.StatusOK, map[string]interface{}{
+		"token": token,
 	})
 }
 
--- internal/delivery/http/routes/user_routes.go
+++ internal/delivery/http/routes/user_routes.go
@@ -10,6 +10,7 @@ import (
 func RegisterUserRoutes(router *mux.Router, uc *usecase.UserUseCase) {
 	userHandler := handler.NewUserHandler(uc)
 
+	router.HandleFunc("/login", userHandler.Login).Methods("POST")
 	router.HandleFunc("/register", userHandler.Register).Methods("POST")
 	router.HandleFunc("/users", userHandler.GetUsers).Methods("GET")
 	router.HandleFunc("/users/{id}", userHandler.GetUserByID).Methods("GET")
