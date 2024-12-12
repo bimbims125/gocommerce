df6f7e581c36dbce8063650ae853f088590e42ba
bimbims125 <pramvendread@gmail.com>
2024-12-12 09:36:33 +0700

    Feat(Cart): get cart by user id

--- internal/delivery/http/routes/cart_routes.go
+++ internal/delivery/http/routes/cart_routes.go
@@ -11,5 +11,6 @@ func RegisterCartRoutes(router *mux.Router, uc *usecase.CartUsecase) {
 	cartHandler := handler.NewCartHandler(uc)
 
 	router.HandleFunc("/carts", cartHandler.CreateCartHandler).Methods("POST")
+	router.HandleFunc("/carts/{user_id}", cartHandler.GetCartByUserIDHandler).Methods("GET")
 	// router.HandleFunc("/carts", cartHandler.GetCarts).Methods("GET")
 }
--- internal/repository/cart_repository.go
+++ internal/repository/cart_repository.go
@@ -23,3 +23,21 @@ func (r *CartRepository) CreateCart(ctx context.Context, cart *entity.Cart) (int
 	}
 	return id, nil
 }
+
+func (r *CartRepository) GetCartByUserID(ctx context.Context, userID int) ([]entity.Cart, error) {
+	query := "SELECT id, user_id, product_id, quantity FROM carts WHERE user_id = $1"
+	rows, err := r.db.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var carts []entity.Cart
+	for rows.Next() {
+		var cart entity.Cart
+		if err := rows.Scan(&cart.ID, &cart.UserID, &cart.ProductID, &cart.Quantity); err != nil {
+			return nil, err
+		}
+		carts = append(carts, cart)
+	}
+	return carts, nil
+}
